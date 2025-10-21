07171991f8b01cc097d714216bf830529f219be8
Yixian Cao <yixian.cao@lam.fr>
2025-10-21 13:55:27 +0200

    Revised setup.py to import the module standalone.

--- gpao_obd/generate_finding_chart.py
+++ gpao_obd/generate_finding_chart.py
@@ -37,29 +37,28 @@ def generate_finding_charts(obdfiles):
 
         png_path = filepath.with_suffix('.png')
 
-        if not png_path.exists():
-            obd = parse_obd(filepath)
-            tel = SkyCoord(template.hms2deg(obd['TEL.TARG.ALPHA'])*degree, template.dms2deg(obd['TEL.TARG.DELTA'])*degree)
-            ra = tel.ra.to(degree).value
-            dec = tel.dec.to(degree).value
-            radius = (60*arcsec).to(degree).value
+        obd = parse_obd(filepath)
+        tel = SkyCoord(template.hms2deg(obd['TEL.TARG.ALPHA'])*degree, template.dms2deg(obd['TEL.TARG.DELTA'])*degree)
+        ra = tel.ra.to(degree).value
+        dec = tel.dec.to(degree).value
+        radius = (60*arcsec).to(degree).value
 
-            resp = requests.get(f'https://gsss.stsci.edu/webservices/vo/CatalogSearch.aspx?RA={ra:.6f}&DEC={dec:.6f}&SR={radius:.3f}')
-            tbl = Table.read(BytesIO(resp.content))
+        resp = requests.get(f'https://gsss.stsci.edu/webservices/vo/CatalogSearch.aspx?RA={ra:.6f}&DEC={dec:.6f}&SR={radius:.3f}')
+        tbl = Table.read(BytesIO(resp.content))
 
-            fig, axarr = pyplot.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
-            for i, mag_name in enumerate(['gaiaRpMag', 'tmassKsMag']):
-                mag = tbl[mag_name]
+        fig, axarr = pyplot.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+        for i, mag_name in enumerate(['gaiaRpMag', 'tmassKsMag']):
+            mag = tbl[mag_name]
 
-                mag2size = lambda mag: 50*(np.max(mag)-mag)/(np.max(mag)-np.min(mag))
-                size2mag = lambda size: np.max(mag)-(size/50)*(np.max(mag)-np.min(mag))
+            mag2size = lambda mag: 50*(np.max(mag)-mag)/(np.max(mag)-np.min(mag))
+            size2mag = lambda size: np.max(mag)-(size/50)*(np.max(mag)-np.min(mag))
 
-                axarr[i].plot(ra+10/3600*np.cos(np.linspace(0, 2*np.pi, 1000)), dec+10/3600*np.sin(np.linspace(0, 2*np.pi, 1000)), 'k', lw=1)
-                axarr[i].plot(ra+ 5/3600*np.cos(np.linspace(0, 2*np.pi, 1000)), dec+ 5/3600*np.sin(np.linspace(0, 2*np.pi, 1000)), 'k', lw=0.5)
-                data = axarr[i].scatter(tbl['ra'], tbl['dec'], mag2size(tbl[mag_name]))
-                axarr[i].set_aspect('equal')
-                axarr[i].legend(*data.legend_elements(prop='sizes', num=5, func=size2mag), title=mag_name, loc='upper right')
-                axarr[i].set_xlim(ra-20/3600, ra+20/3600)
-                axarr[i].set_ylim(dec-20/3600, dec+20/3600)
-            fig.savefig(png_path)
-            pyplot.show()
+            axarr[i].plot(ra+10/3600*np.cos(np.linspace(0, 2*np.pi, 1000)), dec+10/3600*np.sin(np.linspace(0, 2*np.pi, 1000)), 'k', lw=1)
+            axarr[i].plot(ra+ 5/3600*np.cos(np.linspace(0, 2*np.pi, 1000)), dec+ 5/3600*np.sin(np.linspace(0, 2*np.pi, 1000)), 'k', lw=0.5)
+            data = axarr[i].scatter(tbl['ra'], tbl['dec'], mag2size(tbl[mag_name]))
+            axarr[i].set_aspect('equal')
+            axarr[i].legend(*data.legend_elements(prop='sizes', num=5, func=size2mag), title=mag_name, loc='upper right')
+            axarr[i].set_xlim(ra-20/3600, ra+20/3600)
+            axarr[i].set_ylim(dec-20/3600, dec+20/3600)
+        fig.savefig(png_path)
+        pyplot.show()
--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
+readme_path = Path(__file__).with_name("README.md")
+long_description = readme_path.read_text(encoding="utf-8") if readme_path.exists() else ""
+
 setup(
-    name='gpao_obd',
-    version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A project for generating templates and finding charts from astronomical data.',
-    packages=find_packages(where='gpao_obd'),
-    package_dir={'': 'gpao_obd'},
+    name="gpao_obd",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="Generate templates and finding charts from astronomical data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=("tests", "notebooks")),
+    include_package_data=True,
     install_requires=[
-        'astropy',
-        'numpy',
-        'matplotlib',
-        'requests',
+        "astropy",
+        "numpy",
+        "matplotlib",
+        "requests",
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.11',
+    python_requires=">=3.11",
 )
